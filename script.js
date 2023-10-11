f13aa4b7880d424ae7b9c314e7d3e1160ca80d00
AntonHansen <anton.hansen@hotmail.com>
2023-10-11 11:03:18 +0200

    initial lines Anton

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const wordBank = [""]
+// getElementByClass
+
+const words = ["Volvo", "Saab", "Ford", "Fiat", "Ferrari", "Porsche"];
+const randomWords = words[Math.floor(Math.random()*words.length)];
+const splitWords = randomWords.split("");
+const consoleSplitWords = randomWords.split("");
+const consoleJoinSplitWords = consoleSplitWords.join("");
+console.log(consoleSplitWords);
+console.log(consoleJoinSplitWords);
+
+for (let i = 0; i < splitWords.length; i++) {
+    splitWords[i] = "_";
+}
+
+
+const joinSplitWords = splitWords.join("");
+
+// const howMany = splitWords.length("");
+// console.log(howMany);
+// splitWords.splice(0, 1, "_");
+
+
+
+
+
+const seeWords = document.querySelector('#currentWord');
+
+seeWords.innerText = splitWords;
