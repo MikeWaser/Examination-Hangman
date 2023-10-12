ae934ec236f516443852aa085031cc5a142f9a1d
Mike <mik3waser@gmail.com>
2023-10-12 16:41:30 +0200

    12/10 1

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ const joinSplitWords = splitWords.join(" ");
 const seeWords = document.querySelector("#currentWord");
 
 seeWords.innerText = joinSplitWords;
+
+//Timer code /Hampus
+
+const startingMinutes = 5;
+let time = startingMinutes * 60;
+const knapp = document.getElementById("StartTimerBtn");
+const countdownEl = document.getElementById("countdown");
+
+let CountdownInterval;
+
+function startTimer() {
+  CountdownInterval = setInterval(updateCountdown, 1000);
+
+  function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    knapp.disabled = true;
+
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+
+    if (time === -1) {
+      clearInterval(CountdownInterval);
+    }
+  }
+}
