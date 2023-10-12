ce9077ccca14ba6d6faaad832acc5a7aa62caf1a
MikeWaser <mik3waser@gmail.com>
2023-10-12 11:07:55 +0200

    mc

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const wordBank = []
+// Array med ord som ska slumpas in i continer answer
+const wordBank = [
+  "javascript",
+  "website",
+  "game",
+  "keyboard",
+  "hangman",
+  "coding",
+];
+
+const randomAnswer = wordBank[Math.floor(Math.random() * wordBank.length)]
+
+let currentWord = []
+
+for (let i = 0 ; i < randomAnswer.length; i++){
+currentWord.push("_")
+}
+document.getElementById("currentWord2").innerHTML = currentWord.join ("")
