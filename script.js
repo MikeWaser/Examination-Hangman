58555c55b521de77016c10170062df5b22d29c09
Mike <mik3waser@gmail.com>
2023-10-15 03:23:31 +0200

    function testing 1.5

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const terms = [
+// En array med ord som kan användas som målord i hangman-spelet
+const words = [
   "JavaScript",
   "HTML",
   "CSS",
@@ -20,39 +21,52 @@ const terms = [
   "Framework",
   "Syntax",
 ];
-const randomTerm = terms[Math.floor(Math.random() * terms.length)];
+
+// Välj ett slumpmässigt ord från listan
+const randomTerm = words[Math.floor(Math.random() * words.length)];
+
+// Splitta det slumpmässiga ordet till en array av bokstäver
 const splitTerm = randomTerm.split("");
+
+// Skapa en kopia av det slumpmässiga ordet för konsolen
 const consoleSplitTerm = randomTerm.split("");
+
+// Slå ihop det kopiade ordet till en sträng för att visa i konsolen
 const consoleJoinSplitTerm = consoleSplitTerm.join("");
+
+// Visa den splittrade versionen av det slumpmässiga ordet i konsolen
 console.log(consoleSplitTerm);
+
+// Visa den joinade versionen av det splittrade ordet i konsolen
 console.log(consoleJoinSplitTerm);
 
+// Skapa en array med underscores (_), en för varje bokstav i det slumpmässiga ordet
 for (let i = 0; i < splitTerm.length; i++) {
   splitTerm[i] = "_";
 }
 
+// Slå ihop arrayen med underscores till en sträng med mellanslag och visa den på webbsidan
 const joinSplitTerm = splitTerm.join(" ");
-
 const seeWords = document.querySelector("#currentWord");
-
 seeWords.innerText = joinSplitTerm;
 
-// Timer code /Hampus
+// Timer-kod
+const correctGuesses = [];  // Array för korrekta gissningar
+const wrongGuesses = [];    // Array för felaktiga gissningar
 
-const correctGuesses = [];
-const wrongGuesses = [];
-
+// Hämta elementen från HTML
 const userInputField = document.querySelector(".userInput input");
 const currentWordElement = document.getElementById("currentWord");
 const wrongLetterElement = document.getElementById("wrongLetter");
 
-let time = 5 * 60; // 5 minutes
+let time = 5 * 60; // 5 minuter, omvandlat till sekunder
 
 const knapp = document.getElementById("StartTimerBtn");
 const countdownEl = document.getElementById("countdown");
 
 let CountdownInterval;
 
+// Funktion för att starta timer
 function startTimer() {
   CountdownInterval = setInterval(updateCountdown, 10);
 
@@ -63,6 +77,7 @@ function startTimer() {
 
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
+    // Uppdatera nedräkningen i DOM
     countdownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
 
@@ -71,14 +86,16 @@ function startTimer() {
   }
 }
 
+// Funktion för att kontrollera om tiden har gått ut
 function checkTimeOut() {
   if (time === -1) {
     clearInterval(CountdownInterval);
-    // Anropa funktionen för att visa förlustresultat om tiden har gått ut
+    // Visa resultatet för förlust om tiden har gått ut
     handleLossResult(false);
   }
 }
 
+// Lyssna på användarinput
 userInputField.addEventListener("input", function () {
   const userInput = userInputField.value.toLowerCase();
 
@@ -95,7 +112,7 @@ userInputField.addEventListener("input", function () {
           wrongGuesses.push(userInput);
           updateWrongLetterDisplay();
           if (wrongGuesses.length >= 6) {
-            // Anropa funktionen för att visa förlustresultat
+            // Visa resultatet för förlust om användaren har gjort för många felaktiga gissningar
             handleLossResult(false);
           }
         }
@@ -110,6 +127,7 @@ userInputField.addEventListener("input", function () {
   }
 });
 
+// Funktion för att uppdatera visningen av det aktuella ordet
 function updateCurrentWordDisplay() {
   const displayWord = randomTerm
     .split("")
@@ -117,12 +135,13 @@ function updateCurrentWordDisplay() {
     .join(" ");
   currentWordElement.textContent = displayWord;
 
-  // Kolla om användaren har gissat hela ordet
+  // Kolla om användaren har gissat hela ordet korrekt
   if (displayWord.toLowerCase() === randomTerm.toLowerCase()) {
     // Här kan du lägga till kod för att hantera när användaren har gissat hela ordet korrekt.
   }
 }
 
+// Funktion för att uppdatera visningen av felaktiga bokstäver
 function updateWrongLetterDisplay() {
   wrongLetterElement.textContent = wrongGuesses.join(" ");
 }
@@ -150,10 +169,10 @@ function handleLossResult(isWinner) {
   newGameButton.style.display = "block";
 }
 
+// Lyssna på klick på knappen för att starta en ny omgång
 const newGameButton = document.getElementById("newGame");
 newGameButton.addEventListener("click", function () {
   const resultPopup = document.getElementById("resultPopup");
   resultPopup.style.display = "none";
-  location.reload();
+  location.reload(); // Ladda om sidan för att starta en ny omgång
 });
-// Lägg till en händelselyssnare för att start
