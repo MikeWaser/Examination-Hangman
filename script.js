14d8c1a8564f9489e432709e0596cd57f96a4bf5
AntonHansen <anton.hansen@hotmail.com>
2023-10-15 18:29:46 +0200

    Work 15 oct

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// slumpmässigt väljer ett ord från en array.
 function randomWordGenerator() {
   const words = ["Volvo", "Saab", "Ford", "Fiat", "Ferrari", "Porsche"];
   const randomWords = words[Math.floor(Math.random()*words.length)];
   return randomWords;
 }
+
+// en variable för att covered och revealed får samma ord och hämtar inte randomWordsGenerator var för sig.
 let getWord = randomWordGenerator();
+
+// en function för att för att hämta en sträng, ändra alla tecken till understräck och skapa mellanslag mellan varje understräck.
 function coveredLettersWords (okok) {
   const splitWordsCovered = okok;
   const splitWords = splitWordsCovered.split("");
@@ -13,29 +18,37 @@ function coveredLettersWords (okok) {
   const joinSplitWordsCovered = splitWords.join(" ");
   return joinSplitWordsCovered;
 }
+
+// en function för att för att hämta en sträng och skapa mellanslag mellan varje tecken.
 function revealingLettersWords (noknok) {
   const splitWordsRevealed = (noknok);
   const splitWords = splitWordsRevealed.split("");
   const joinSplitWordsRevealed = splitWords.join(" ");
   return joinSplitWordsRevealed;
 }
-console.log(coveredLettersWords(getWord));
-console.log(revealingLettersWords(getWord));
 
-
+const seeWords = document.querySelector("#currentWord");
+seeWords.innerText = coveredLettersWords(getWord);
 
 // splitWords.splice(0, 1, "_");
 
-const seeWords = document.querySelector("#currentWord");
-
-seeWords.innerText = randomWordGenerator();
 
 let guessLetter = document.querySelector("#guessLetter")
 let showLetter = document.querySelector("#wrongLetter")
+
 guessLetter.addEventListener("input", () => { // (guessLetter elr inputValue == randomWordGenerator.includes "maybe")
   let inputValue = guessLetter.value;
   showLetter.innerText = inputValue;
 });
+
+
+/***************************************************************************************************************************************************/
+/***************************************************************************************************************************************************/
+/***************************************************************************************************************************************************/
+// Alla console.logs
+console.log(revealingLettersWords(getWord));
+console.log(coveredLettersWords(getWord));
+
 
 /*
 
@@ -54,86 +67,3 @@ function () {
 
 // funktion som kollar words lista och guesses lista 
 
-
-
-/*********************************************************************************************/
-/*********************************************************************************************/
-/*********************************************************************************************/
-
-
-
-
-// function randomWordGenerator() {
-//   const words = ["Volvo", "Saab", "Ford", "Fiat", "Ferrari", "Porsche"];
-//   const randomWords = words[Math.floor(Math.random()*words.length)];
-//   // const splitWords = randomWords.split("");
-//   return randomWords;
-// }
-// let getWord = randomWordGenerator();
-// console.log(getWord);
-// console.log(typeof getWord);
-
-// function coveredLettersWords (okok) {
-//  /*  const splitWordsCovered = okok;
-//   let result = splitWordsCovered.split("");
-//   console.log(result);
-//   let result2 = result.join("_")
-//   console.log("kalle",result2)
-
-// return result2; */
-// const my_string = okok
-
-// let pattern = my_string
-// let replacement = "_";
-
-// let my_new_string = my_string.replaceAll(pattern,replacement);
-
-// console.log(my_new_string);
-
-// }
-
-// function revealingLettersWords (noknok) {
-//   const splitWordsRevealed = (noknok);
-//   // const joinSplitWordsRevealed = splitWordsRevealed.join(" ");
-//   return splitWordsRevealed;
-// }
-
-// console.log(coveredLettersWords(getWord));
-// console.log(revealingLettersWords(getWord));
-
-// /* {
-//   const splitWordsCovered = splitWords();
-  
-  
-  
-// } */
-
-// // splitWords.splice(0, 1, "_");
-
-// const seeWords = document.querySelector("#currentWord");
-
-// seeWords.innerText = randomWordGenerator();
-
-// let guessLetter = document.querySelector("#guessLetter")
-// let showLetter = document.querySelector("#wrongLetter")
-// guessLetter.addEventListener("input", () => { // (guessLetter elr inputValue == randomWordGenerator.includes "maybe")
-//   let inputValue = guessLetter.value;
-//   showLetter.innerText = inputValue;
-// });
-
-// /*
-
-// function () {
-//   if (input == randomWordGenerator.includes) {
-//     True (reveal letter)
-//   } else {
-//     False (add svg path)
-//   }
-// }
-
-// */
-
-// /* if sats som gör en .includes() koll med nån loop från "Guess Letter". TRUE: .IndexOf() för det korrekt gissade blir replaced med ordinarie bokstav (med .splice() kanske), FALSE: returnar att du gissade fel och tar ett steg i listan/arrayen för SVG filen och emot failstate. */
-
-
-// // funktion som kollar words lista och guesses lista 
